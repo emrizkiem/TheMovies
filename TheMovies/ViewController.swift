68a32727214f9e6d6a881cc41d08890ceb5717b4
M. Rizki Maulana <devemrizkiem@gmail.com>
2025-03-17 22:57:17 +0700

    Initial Commit

---
+++ TheMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovies
+//
+//  Created by M. Rizki Maulana on 17/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ TheMoviesTests/TheMoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheMoviesTests.swift
+//  TheMoviesTests
+//
+//  Created by M. Rizki Maulana on 17/03/25.
+//
+
+import Testing
+@testable import TheMovies
+
+struct TheMoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
